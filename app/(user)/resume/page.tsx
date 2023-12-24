a73acd0de618b04e07710e912f3a9bfc9dc50de0
Alcatraz-Dev <alcatraz3019@gmail.com>
2023-12-24 13:13:37 +0100

    fis errors

--- app/(user)/resume/page.tsx
+++ app/(user)/resume/page.tsx
@@ -2,9 +2,9 @@
 import React from "react";
 import { client } from "@/lib/sanity.client";
 import { groq } from "next-sanity";
-import { previewData } from "next/headers";
+
 import PreviewSuspense from "@/components/PreviewSuspense";
-import ResumePreview from "@/components/PreviewsComponents/PreviewResume/ResumePreview"
+
 import ResumePage from "@/components/Resume/Resume";
 
 
@@ -17,21 +17,7 @@ const resumeQuery = groq`
 
 export const revalidate = 10;
 export default async function Resume() {
-  if (previewData()) {
-    return (
-      <PreviewSuspense
-        fallback={
-          <div role={"status"}>
-            <p className="text-center text-lg animate-pulse text-[#99e000]">
-              Loading Preview Data...
-            </p>
-          </div>
-        }
-      >
- <ResumePreview resume={resumeQuery} />
-      </PreviewSuspense>
-    )
-  }
+ 
   const resume = await client.fetch(resumeQuery);
 return (
   <>
